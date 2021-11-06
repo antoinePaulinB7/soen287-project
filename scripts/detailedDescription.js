43de13f6ed33189b228624b262d38480e233bca0
OIS202 <o.i.m.ab.s.a@gmail.com>
2021-11-06 07:55:33 -0400

    Detailed Description
    Detailed Description for every functioning p3 page.
    By Omar

---
+++ scripts/detailedDescription.js
@@ -0,0 +1,6 @@
+function showIt() {
+    document.getElementById("theDetailedDescription").style.display = "block";
+}
+function hideIt() {
+    document.getElementById("theDetailedDescription").style.display = "none";
+}
